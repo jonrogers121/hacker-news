0e0467cb87d1630ed158dc8da3acd3486c4b4d37
Jon Rogers <jonathan.rogers@concreteplatform.com>
2018-11-03 16:00:01 +0000

    added axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import axiosWrapper from "./api";
 
 class App extends Component {
+
+  componentDidMount(){
+      axiosWrapper.get('jaws').then(
+          (res) => {
+              console.log(res.data)
+          },
+      );
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export default {
+    get: (t) =>
+        axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: '3ef5089a',
+                t,
+            }
+        }).then((response) => {
+            return response
+        }).catch((error) => {
+                console.log(error);
+            }
+        ),
+};
+
